17ca1d02f824eb65c6b6a1b7de1976a048f65bb2
Ravishan-dev <ravishansamarasinghe31@gmail.com>
2025-12-23 23:17:17 +0530

    Logic and insertion User sign-up

---
+++ src/main/java/com/ravishandev/epicreads/service/UserService.java
@@ -0,0 +1,82 @@
+package com.ravishandev.epicreads.service;
+
+import com.google.gson.JsonObject;
+import com.ravishandev.epicreads.dto.UserDTO;
+import com.ravishandev.epicreads.entity.Status;
+import com.ravishandev.epicreads.entity.User;
+import com.ravishandev.epicreads.util.AppUtil;
+import com.ravishandev.epicreads.util.HibernateUtil;
+import com.ravishandev.epicreads.validation.Validator;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class UserService {
+
+    public static String createAccount(UserDTO userDTO) {
+
+        JsonObject responseObject = new JsonObject();
+        boolean status = false;
+        String message = "";
+
+        if (userDTO.getFirstName() == null) {
+            message = "First Name is Required";
+        } else if (userDTO.getFirstName().isBlank()) {
+            message = "Fist Name can not be Empty";
+        } else if (userDTO.getLastName() == null) {
+            message = "Last Name id Required";
+        } else if (userDTO.getLastName().isBlank()) {
+            message = "Last Name can not be Empty";
+        } else if (userDTO.getEmail() == null) {
+            message = "Email is Required";
+        } else if (userDTO.getEmail().isBlank()) {
+            message = "Email can not be Empty";
+        } else if (!userDTO.getEmail().matches(Validator.EMAIL_VALIDATION)) {
+            message = "Please Provide a Valid Email Address";
+        } else if (userDTO.getPassword() == null) {
+            message = "Password is Required";
+        } else if (userDTO.getPassword().isBlank()) {
+            message = "Password can not be Empty";
+        } else if (!userDTO.getPassword().matches(Validator.PASSWORD_VALIDATION)) {
+            message = "Please provide valid password. \n " +
+                    "The password must be at least 8 characters long and include at least one uppercase letter, " +
+                    "one lowercase letter, one digit, and one special character";
+        } else {
+            Session hibernateSession = HibernateUtil.getSessionFactory().openSession();
+            User singleUser = hibernateSession.createNamedQuery("User.getByEmail", User.class)
+                    .setParameter("email", userDTO.getEmail())
+                    .getSingleResultOrNull();
+            if (singleUser != null) {
+                message = "This Email Already Registered... Please Enter Another Email Address";
+            } else {
+                User user = new User();
+                user.setFirstName(userDTO.getFirstName());
+                user.setLastName(userDTO.getLastName());
+                user.setEmail(userDTO.getEmail());
+                user.setPassword(userDTO.getPassword());
+
+                Status pendingStatus = hibernateSession.createNamedQuery("Status.findByValue", Status.class)
+                        .setParameter("value", String.valueOf(Status.type.PENDING))
+                        .getSingleResultOrNull();
+
+                user.setStatus(pendingStatus);
+
+                Transaction transaction = hibernateSession.beginTransaction();
+                try {
+                    hibernateSession.persist(user);
+                    transaction.commit();
+                    status = true;
+                    message = "Account Creation Successful";
+                } catch (HibernateException e) {
+                    transaction.rollback();
+                    message = "Account Creation Failed...Please Try Again";
+                }
+            }
+            hibernateSession.close();
+        }
+
+        responseObject.addProperty("status", status);
+        responseObject.addProperty("message", message);
+        return AppUtil.GSON.toJson(responseObject);
+    }
+}
